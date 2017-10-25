35030a88db15fd58340e2c381da65095e217d45b
Ha Junsoo <kuc2477@gmail.com>
2017-10-25 17:57:41 +0900

    add main skeleton

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 #!/usr/bin/env python3
+import argparse
+
+
+parser = argparse.ArgumentParser('PyTorch Implementation of WGAN-GP')
+parser.add_argument()
+parser.add_argument()
+parser.add_argument()
+parser.add_argument()
+parser.add_argument()
+parser.add_argument()
+
+
+if __name__ == '__main__':
+    args = parser.parse_args()
